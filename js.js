595b6b2f83be5d441866c5a8edcc361c7d652989
Peter Cho <pcho231@gmail.com>
2016-11-13 04:07:21 -0500

    Solution to function

--- js.js
+++ js.js
@@ -1,21 +1,29 @@
-var integerArray = [], indices = [];
+var integerArray = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 7], indices = [];
 
 function addInteger(e) {
-    integerValue = document.getElementById('numberInput').value;
-    integerArray.push(integerValue);
-    document.getElementById('integerArray').innerHTML += (integerArray.length == 1 ? "" : "," ) + integerValue;
-    document.getElementById("numberInput").value = "";
-
+  integerValue = document.getElementById('numberInput').value;
+  integerArray.push(integerValue);
+  document.getElementById('integerArray').innerHTML += (integerArray.length == 1 ? "" : "," ) + integerValue;
+  document.getElementById("numberInput").value = "";
 };
 
 var reset = function () {
-    document.getElementById("indicesArray").innerHTML = "";
-    document.getElementById('integerArray').innerHTML = "";
-    integerArray = [];
-    indices = [];
-
+  document.getElementById("indicesArray").innerHTML = "";
+  document.getElementById('integerArray').innerHTML = "";
+  integerArray = [];
+  indices = [];
 };
 
 function searchConsecutiveNumbers() {
-    console.log("To be implemented");
+  var diff1, diff2;
+
+  for(var i = 0; i < integerArray.length; i += 1) {
+    diff1 = integerArray[i] - integerArray[i + 1];
+    diff2 = integerArray[i + 1] - integerArray[i + 2];
+    if ((diff1 == 1 || diff1 == -1) && diff1 === diff2) {
+      indices.push(i);
+    }
+  }
+
+  document.getElementById("indicesArray").innerHTML += (indices.length == 1 ? "" : "," ) + indices;
 };
