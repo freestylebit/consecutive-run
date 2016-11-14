24384948671dfa322c9da55fba01c0fb0f77f7a9
Peter Cho <pcho231@gmail.com>
2016-11-13 22:59:05 -0500

    Reset button, ported function

--- src/code.js
+++ src/code.js
@@ -41,18 +41,20 @@ import ReactDOM from 'react-dom';
 class ConsecutiveRun extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       integerArray: [],
       indices: [],
       pending: '',
-      error: '',
+      error: ''
     };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleChange(event) {
-    this.addInteger();
     this.setState({
       pending: event.target.value
     });
@@ -60,31 +62,73 @@ class ConsecutiveRun extends React.Component {
 
   handleSubmit(event) {
     let n = this.state.pending;
-    if (!isNaN(parseFloat(n)) && isFinite(n)) {
-      let temp = this.state.integerArray;
-      temp.push(this.state.pending);
-      this.setState({
-        integerArray: temp,
-        pending: '',
-        error: ''
-      });
+    if (n) {
+      if (!isNaN(parseFloat(n)) && isFinite(n)) {
+        let temp = this.state.integerArray;
+        temp.push(this.state.pending);
+        this.setState({
+          integerArray: temp,
+          pending: '',
+          error: ''
+        });
+      }
+      else {
+        this.setState({
+          error: 'Not a number'
+        });
+      }
     }
-    else {
-      this.setState({
-        error: 'Not a number'
-      });
-    }
+
+    this.searchConsecutiveNumbers();
 
     event.preventDefault();
   }
 
-  addInteger() {
+  handleReset() {
+    this.setState({
+      integerArray: [],
+      indices: [],
+      pending: '',
+      error: ''
+    });
+
+    this.focusInput();
   }
 
   componentDidMount() {
-    // Focus on input box.
+    this.focusInput();
+  }
+
+  // Focus on input box for convenience.
+  focusInput() {
     document.getElementById('numberInput').focus();
   }
+
+  // Calculate the runs.
+  searchConsecutiveNumbers() {
+    let diff1, diff2;
+
+    let list = this.state.integerArray;
+    let indices = [];
+
+    for (let i = 0; i < list.length; i += 1) {
+      diff1 = list[i] - list[i + 1];
+      diff2 = list[i + 1] - list[i + 2];
+      if ((diff1 == 1 || diff1 == -1) && diff1 === diff2) {
+        indices.push(i);
+      }
+    }
+
+    this.setState({
+      indices: indices
+    });
+
+    console.log(this.state.indices);
+
+    if (indices.length == 0) {
+      //document.getElementById("indicesArray").innerHTML = 'No results found';
+    }
+  };
 
   render() {
     return (
@@ -93,7 +137,7 @@ class ConsecutiveRun extends React.Component {
         <p>{this.state.error}</p>
         <form onSubmit={this.handleSubmit}>
           Enter the integer one by one: <input type="text" id="numberInput" value={this.state.pending} onChange={this.handleChange} />
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Submit" /> <button onClick={this.handleReset}>Reset</button>
         </form>
         <div className="runs__tally">
           <div className="runs__tally__confirmed">
@@ -101,9 +145,10 @@ class ConsecutiveRun extends React.Component {
               {this.state.integerArray.map(function(v, i) {
                 return <li key={i}>{v}</li>;
               })}
+              <li>{this.state.pending}</li>
             </ul>
           </div>
-          <div className="runs__tally__pending">{this.state.pending}</div>
+          <div className="runs__tally__results">{this.state.indices}</div>
         </div>
       </div>
     );
